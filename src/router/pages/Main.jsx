49e21ea4db6c2207a8109f694dbcc5bf2184ae40
maantano <maantano@naver.com>
2024-01-23 02:04:59 +0900

    env 수정

--- src/router/pages/Main.jsx
+++ src/router/pages/Main.jsx
@@ -41,6 +41,7 @@ const Main = () => {
 
   // firebase 마이그레이션 randomCard ========================================================
   async function randomCard() {
+    console.log("REACT_APP_TEST ===>", process.env.REACT_APP_TEST);
     console.log(process.env.REACT_APP_HOST);
     console.log("process.env.REACT_APP_HOST ===>", process.env.REACT_APP_HOST);
     try {
